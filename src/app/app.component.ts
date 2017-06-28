b6ef4a556f62ae99079946bb871951782e9b5133
Vicente <pouyio@gmail.com>
2017-06-28 23:53:25 +0200

    nav links dinamycally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { appRoutes } from './app.module';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor() {}
+  routes: Routes;
+  constructor() {
+    this.routes = appRoutes;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { AppComponent } from './app.component';
 
 import {WarsService} from './api/wars.service';
 import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
-const appRoutes: Routes = [
-  { path: 'demo-1', component: Demo1Component }
+export const appRoutes: Routes = [
+  { path: 'demo-1', component: Demo1Component },
+  { path: 'demo-2', component: Demo2Component },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    Demo1Component
+    Demo1Component,
+    Demo2Component
   ],
   imports: [
     BrowserModule,
