77699fbd7075a27340c00fed0e15f2d061f903d7
Vicente <pouyio@gmail.com>
2017-06-28 23:54:49 +0200

    added demos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,23 @@ import { AppComponent } from './app.component';
 import {WarsService} from './api/wars.service';
 import { Demo1Component } from './demo1/demo1.component';
 import { Demo2Component } from './demo2/demo2.component';
+import { Demo3Component } from './demo3/demo3.component';
+import { Demo4Component } from './demo4/demo4.component';
 
 export const appRoutes: Routes = [
   { path: 'demo-1', component: Demo1Component },
   { path: 'demo-2', component: Demo2Component },
+  { path: 'demo-3', component: Demo3Component },
+  { path: 'demo-4', component: Demo4Component },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     Demo1Component,
-    Demo2Component
+    Demo2Component,
+    Demo3Component,
+    Demo4Component
   ],
   imports: [
     BrowserModule,
